e16a1ebb89e4469f45ab57de467ffe0e1d2d882e
Adrian Pascu <mail@adipascu.ro>
2023-08-04 18:30:40 +0200

    Add settings functionality

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,3 +1,32 @@
+import { createSignal } from "solid-js";
+import { setBirthDay } from "./storage";
+
 export default () => {
-  return "Settings";
+  const [date, setDate] = createSignal<Date | null>(null);
+  return (
+    <div>
+      <input
+        type="date"
+        onInput={(e) => {
+          const date = new Date(e.currentTarget.value);
+          if (isNaN(date.getTime())) {
+            return;
+          }
+          setDate(date);
+        }}
+      />
+      <button
+        disabled={date() === null}
+        onClick={() => {
+          const inputDate = date();
+          if (!inputDate) {
+            throw new Error("Birthday not set");
+          }
+          setBirthDay(inputDate);
+        }}
+      >
+        Motivate
+      </button>
+    </div>
+  );
 };
