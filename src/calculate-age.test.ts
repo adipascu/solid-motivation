705c1cb298d2405698ee1ec0552a34d3a3b5ddfb
Adrian Pascu <mail@adipascu.ro>
2023-09-20 17:53:42 +0200

    Simplify calculateAge function

--- src/calculate-age.test.ts
+++ src/calculate-age.test.ts
@@ -4,29 +4,31 @@ import { calculateAge } from "./calculate-age";
 describe("calculateAge", () => {
   it("calculates age correctly for UTC", () => {
     const birthDate = Temporal.PlainDate.from("2000-01-01");
-    const now = Temporal.Instant.from("2020-01-01T00:00:00Z");
-    const utcTimezone = Temporal.TimeZone.from("UTC");
-    expect(calculateAge(birthDate, now, utcTimezone)).toBe(20);
+    const now = Temporal.ZonedDateTime.from("2020-01-01T00:00:00Z[UTC]");
+    expect(calculateAge(birthDate, now)).toBe(20);
   });
 
   it("calculates age correctly for New York (EST)", () => {
     const birthDate = Temporal.PlainDate.from("2000-01-01");
-    const now = Temporal.Instant.from("2020-01-01T00:00:00-05:00");
-    const nyTimezone = Temporal.TimeZone.from("America/New_York");
-    expect(calculateAge(birthDate, now, nyTimezone)).toBe(20);
+    const now = Temporal.ZonedDateTime.from(
+      "2020-01-01T00:00:00-05:00[America/New_York]",
+    );
+    expect(calculateAge(birthDate, now)).toBe(20);
   });
 
   it("calculates age correctly for Tokyo (JST)", () => {
     const birthDate = Temporal.PlainDate.from("2000-01-01");
-    const now = Temporal.Instant.from("2020-01-01T00:00:00+09:00");
-    const tokyoTimezone = Temporal.TimeZone.from("Asia/Tokyo");
-    expect(calculateAge(birthDate, now, tokyoTimezone)).toBe(20);
+    const now = Temporal.ZonedDateTime.from(
+      "2020-01-01T00:00:00+09:00[Asia/Tokyo]",
+    );
+    expect(calculateAge(birthDate, now)).toBe(20);
   });
 
   it("calculates age correctly for Auckland (NZDT)", () => {
     const birthDate = Temporal.PlainDate.from("2000-01-01");
-    const now = Temporal.Instant.from("2020-01-01T00:00:00+13:00");
-    const aucklandTimezone = Temporal.TimeZone.from("Pacific/Auckland");
-    expect(calculateAge(birthDate, now, aucklandTimezone)).toBe(20);
+    const now = Temporal.ZonedDateTime.from(
+      "2020-01-01T00:00:00+13:00[Pacific/Auckland]",
+    );
+    expect(calculateAge(birthDate, now)).toBe(20);
   });
 });
--- src/calculate-age.ts
+++ src/calculate-age.ts
@@ -2,20 +2,16 @@ import { Temporal } from "temporal-polyfill";
 
 export const calculateAge = (
   birthDay: Temporal.PlainDate,
-  now: Temporal.Instant,
-  rolloverTimezone: Temporal.TimeZone | Temporal.TimeZoneProtocol,
+  now: Temporal.ZonedDateTime,
 ) => {
-  const midnightInstant = birthDay
-    .toZonedDateTime(rolloverTimezone)
-    .toInstant();
-  return now.since(midnightInstant).total({
+  const midnightInstant = birthDay.toZonedDateTime(now.timeZone).toInstant();
+  return now.toInstant().since(midnightInstant).total({
     unit: "years",
     relativeTo: birthDay,
   });
 };
 
 export const calculateAgeLocal = (birthDay: Temporal.PlainDate) => {
-  const localTime = Temporal.Now.instant();
-  const localTimezone = Temporal.Now.timeZone();
-  return calculateAge(birthDay, localTime, localTimezone);
+  const localTime = Temporal.Now.zonedDateTime("gregory");
+  return calculateAge(birthDay.withCalendar("gregory"), localTime);
 };
