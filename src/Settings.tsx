b61b59d9d18b621457b5674bee25423c2de96a9d
Adrian Pascu <mail@adipascu.ro>
2023-10-13 00:36:37 +0200

    Style settings menu

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,5 +1,7 @@
 import { createSignal } from "solid-js";
 import { Temporal } from "temporal-polyfill";
+import FONT_FAMILY from "./font";
+import getDarkMode from "./dark-signal";
 
 export default ({
   onBirthDay,
@@ -7,11 +9,41 @@ export default ({
   onBirthDay: (birthDay: Temporal.PlainDate | null) => void;
 }) => {
   const [date, setDate] = createSignal<Temporal.PlainDate | null>(null);
+  const colorLabel = () => (getDarkMode() ? "#b9b3aa" : "#b0b5b9");
+  const colorCounter = () => (getDarkMode() ? "#bab4ab" : "#494949");
+  const colorBackground = () => (getDarkMode() ? "#181a1b" : "#ffffff");
+
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        "flex-direction": "column",
+        "align-items": "center",
+        "justify-content": "center",
+      }}
+    >
+      <label
+        style={{
+          "font-family": FONT_FAMILY,
+          "font-size": "19.2px",
+          "font-weight": "bold",
+          color: colorCounter(),
+          "margin-bottom": "4px",
+        }}
+      >
+        Enter your Birthday
+      </label>
       <input
         autofocus
         type="date"
+        style={{
+          "margin-bottom": "10px",
+          padding: "10px",
+          "font-size": "16px",
+          border: "2px solid",
+          "border-color": colorLabel(),
+          "border-radius": "4px",
+        }}
         onKeyDown={(e) => {
           if (e.key === "Enter") {
             const inputDate = date();
@@ -32,6 +64,23 @@ export default ({
             throw new Error("Birthday not set");
           }
           onBirthDay(inputDate);
+        }}
+        style={{
+          padding: "10px 20px",
+          "font-size": "16px",
+          "font-weight": "bold",
+          "border-radius": "4px",
+          border: "none",
+          "background-color": colorCounter(),
+          color: colorBackground(),
+          cursor: "pointer",
+          transition: "background-color 0.2s ease",
+        }}
+        onMouseOver={(e) => {
+          e.currentTarget.style.backgroundColor = colorCounter();
+        }}
+        onMouseOut={(e) => {
+          e.currentTarget.style.backgroundColor = colorLabel();
         }}
       >
         Motivate
